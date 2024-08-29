0a21ce62e090095681d620c27c80cca17402f78d
Ardra KS <sukumaranardra@gmail.com>
2024-08-29 08:51:17 +0530

    updated login page and URLs

--- RescueHub/AidTogether/urls.py
+++ RescueHub/AidTogether/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 from .import views
 
 urlpatterns=[
-    path(' ',views.index,name="index"),
-    path('contact',views.contact,name="contact")
+    path('',views.index,name="index"),
+    path('contact',views.contact,name="contact"),
+    path('login',views.login,name="login")
 ]
--- RescueHub/AidTogether/views.py
+++ RescueHub/AidTogether/views.py
@@ -6,3 +6,5 @@ def index(request):
     return render(request, 'index.html',context={})
 def contact(request):
     return render(request, 'contact.html',context={})
+def login(request):
+    return render(request, 'login.html',context={})
