0358dee6ac756ce9468bed1f958c40a888761ed8
riyaahh <142788178+riyaahh@users.noreply.github.com>
2024-09-04 22:10:49 +0530

    updated relief camp

--- RescueHub/ReliefCamps/urls.py
+++ RescueHub/ReliefCamps/urls.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 from django.urls import path
-from . import views
-
-urlpatterns = [
-]
-=======
-from django.urls import path,include
 
 from .import views
 
-urlpatterns=[
-   
+urlpatterns = [
+    path('CampPortal.html',views.CampPortal, name="CampPortal"),
 ]
->>>>>>> 42665a3deb48b0701cee9444e1e1307d34efa453
+
--- RescueHub/Volunteers/urls.py
+++ RescueHub/Volunteers/urls.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-]
-=======
 from django.urls import path,include
 
 from .import views
@@ -14,4 +6,3 @@ urlpatterns=[
     path('VolunteerPortal',views.VolunteerPortal,name="VolunteerPortal"),
     path('volunteertasks',views.volunteertasks,name="volunteertasks"),
 ]
->>>>>>> 42665a3deb48b0701cee9444e1e1307d34efa453
