e9348128fd116316107e65001ca61eef9a97e9e7
Ardra KS <sukumaranardra@gmail.com>
2024-09-05 23:19:14 +0530

    Updated CSS and set the Registration page

--- RescueHub/ReliefCamps/urls.py
+++ RescueHub/ReliefCamps/urls.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
-
-urlpatterns = [
-]
-=======
 from django.urls import path,include
 
 from .import views
@@ -12,4 +5,3 @@ from .import views
 urlpatterns=[
    
 ]
->>>>>>> 42665a3deb48b0701cee9444e1e1307d34efa453
