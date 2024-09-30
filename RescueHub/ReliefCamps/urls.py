0736d7d43e7759d9fb0571ec2404a02e774a7657
Ardra KS <sukumaranardra@gmail.com>
2024-10-01 00:49:41 +0530

    create request form

--- RescueHub/ReliefCamps/urls.py
+++ RescueHub/ReliefCamps/urls.py
@@ -4,4 +4,5 @@ from .import views
 
 urlpatterns = [
     path('CampPortal',views.CampPortal, name="CampPortal"),
+    path('RequestForm',views.RequestForm, name="RequestForm"),
 ]
--- RescueHub/ReliefCamps/views.py
+++ RescueHub/ReliefCamps/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def CampPortal(request):
     return render(request,"ReliefCamps/CampPortal.html",context={})
+def RequestForm(request):
+    return render(request,"ReliefCamps/RequestForm.html",context={})
