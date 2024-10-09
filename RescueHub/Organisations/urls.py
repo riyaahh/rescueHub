3673a7cc194714ec81e15810851cf4e87909317d
riyaahh <142788178+riyaahh@users.noreply.github.com>
2024-10-09 08:04:48 +0530

    final updated

--- RescueHub/Organisations/urls.py
+++ RescueHub/Organisations/urls.py
@@ -6,11 +6,7 @@ from .import views
 urlpatterns=[
     path('organisationPortal',views.organisationPortal, name="organisationPortal"),
     path('ReqTable',views.ReqTable, name="ReqTable"),
-<<<<<<< HEAD
-]
-=======
     path('denyRequest/<int:id>', views.denyRequest, name="denyRequest"),
     path('acceptRequest/<int:id>', views.acceptRequest, name="acceptRequest"),
 
 ]
->>>>>>> ede9c905eaf6ca80a8d0a64dbc3f16cfb6b3d18b
