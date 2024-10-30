91f6ec23297284118e6446859285f0e5301c5782
Ardra KS <sukumaranardra@gmail.com>
2024-10-30 17:48:35 +0530

    updated status option for volunteers and organisations

--- RescueHub/Volunteers/models.py
+++ RescueHub/Volunteers/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 from ReliefCamps.models import ResourceRequest
 from AidTogether.models import OrganisationProfile
 
@@ -7,7 +9,7 @@ class VolunteerTasks(models.Model):
     receivers_id=models.ForeignKey(ResourceRequest,on_delete=models.CASCADE)
     org_id=models.ForeignKey(OrganisationProfile,on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=[("Pending", "Pending"), ("Accepted", "Accepted"), ("Denied", "Denied")], default="Pending")
-    
+    accepted_by = models.ManyToManyField(User, blank=True, related_name="accepted_tasks")  # Allows multiple users
 
     def __str__(self):
         return f"Request by {self.receivers_id} by {self.org_id}"
--- RescueHub/Volunteers/views.py
+++ RescueHub/Volunteers/views.py
@@ -20,12 +20,21 @@ def denyTask(request, id):
     resource_request.save()  # Save the updated status
     return redirect("volunteerTasks")
 
+@login_required
 def acceptTask(request, id):
     resource_request = get_object_or_404(VolunteerTasks, id=id)
-    resource_request.status = "Accepted"  # Update the status to "Accepted"
-    resource_request.save() 
-    return redirect("volunteerTasks")
     
+    # Check if the user hasn't accepted the task already
+    if not resource_request.accepted_by.filter(id=request.user.id).exists():
+        resource_request.accepted_by.add(request.user)  # Add current user to accepted_by
+        resource_request.status = "Accepted"  # Update the status to "Accepted"
+        resource_request.save()  # Save the updated task
+    
+    return redirect("volunteerTasks")  # Redirect to tasks page
+    
+def DetailedTask(request):   
+    tasks = VolunteerTasks.objects.all()
+    return render(request, 'Volunteers/DetailedTask.html',{'tasks':tasks}) 
 
 
 #
