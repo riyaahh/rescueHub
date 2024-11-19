9190cdf5909de77aa7fb9ca0097cb95486aae7b0
riyaahh <142788178+riyaahh@users.noreply.github.com>
2024-11-19 20:24:49 +0530

    dashboard

--- RescueHub/Volunteers/urls.py
+++ RescueHub/Volunteers/urls.py
@@ -6,7 +6,7 @@ urlpatterns=[
     path('VolunteerPortal',views.VolunteerPortal,name="VolunteerPortal"),
     path('cardView',views.card,name="cardView"),
     path('volunteertasks', views.volunteertasks, name="volunteertasks"),
-    path('denyTask/<int:id>', views.denyTask, name="denyTask"), 
+    # path('denyTask/<int:id>', views.denyTask, name="denyTask"), 
     path('card',views.card,name="card"),
     path('acceptTask/<int:request_id>', views.volunteer_accept_request, name='volunteer_accept_request'),
 ]
