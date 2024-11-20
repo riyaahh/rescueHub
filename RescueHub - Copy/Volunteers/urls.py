d6cbccee6d44ed4d7186c5306939a8540c827816
Ardra KS <sukumaranardra@gmail.com>
2024-11-20 21:01:38 +0530

    accepting volunteers

--- RescueHub - Copy/Volunteers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-
-from .import views
-
-urlpatterns=[
-    path('VolunteerPortal',views.VolunteerPortal,name="VolunteerPortal"),
-    path('volunteertasks',views.volunteertasks,name="volunteertasks"),
-]
